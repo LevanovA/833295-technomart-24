b87a679242549cf3e3914782c2b322a8e1460f26
LevanovA <46813895+LevanovA@users.noreply.github.com>
2019-03-27 23:10:13 +0300

    Проект готов для третьей попытки

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,39 @@
 var link = document.querySelector(".button-map");
 var popup = document.querySelector(".modal-letter");
 var close = document.querySelector(".modal-close");
-var buy = document.querySelector(".button-green-buy");
-var basket = document.querySelector("modal-productbasket");
 
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("modal-show");
-});
+var buyBtns = document.querySelectorAll(".button-green-buy");
+var basket = document.querySelector(".modal-productbasket");
+var nextBuy = document.querySelector(".button-order");
 
-buy.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  basket.classList.add("modal-show");
-});
+if (link) {
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+  });
 
-close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal-show");
-});
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+  });
+
+} if (buyBtns) {
+
+  buyBtns.forEach(function (buyBtn) {
+    buyBtn.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      basket.classList.add("modal-show");
+    });
+  });
+
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    basket.classList.remove("modal-show");
+  });
+
+  nextBuy.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    basket.classList.remove("modal-show");
+  });
+}
+
